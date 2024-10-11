fd4298c2fd0fadd1b2723a8670bd72bd577ec701
AbdurRahman2004 <ar978874@gmail.com>
2024-10-11 23:52:05 +0530

    11/10/24

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,8 @@ import PrivateRoutes from './utils/PrivateRoutes.jsx';
 import RoleBasedRoutes from './utils/RoleBasedRoutes.jsx';
 import AdminSummary from './components/dashboard/AdminSummary.jsx';
 import DepartmentList from './components/department/DepartmentList.jsx';
+import AddDepartment from './components/department/AddDepartment.jsx';
+import EditDepartment from './components/department/EditDepartment.jsx';
 
 
 
@@ -33,8 +35,12 @@ function App() {
           </PrivateRoutes>}>
             <Route index element={<AdminSummary />}>
             </Route>
-            <Route path='/admin-dashboard/departments' element={<DepartmentList />}>
-            </Route>
+            <Route path='/admin-dashboard/departments' element={<DepartmentList />}> </Route>
+            <Route path='/admin-dashboard/add-department' element={<AddDepartment />}></Route>
+            <Route path='/admin-dashboard/department/:id' element={<EditDepartment />}></Route>
+
+
+      
           </Route>
         <Route path='/employee-dashboard' element={<EmployeeDaashboard />}></Route>
         
---
+++ server/controllers/departmentController.js
@@ -0,0 +1,67 @@
+import Department from "../models/Department.js";
+
+const getDepartments = async (req,res) => {
+    try{
+        const departments = await Department.find() 
+        return res.status(200).json({success: true , departments})
+    }catch(error){
+        return res.status(500).json({success:false , error: 'Get department server error'})
+    }
+    }
+    
+
+const addDepartment = async (req,res) => {
+   try{
+     const {dep_name , description} = req.body;
+     const newDep = new Department({
+        dep_name,
+        description
+     })
+     await newDep.save();
+     return res.status(200).json({success:true , department:newDep})
+   }catch(error){
+    return res.status(500).json({success : false ,error : " Add department server error"})
+   }
+}
+
+const getDepartment = async (req,res) => { 
+    try{
+      const {id} = req.params;
+      const department = await Department.findById({_id:id})
+      return res.status(200).json({success:true,department})
+
+    }catch(error){
+       return res.status(500).json({success:false,eerror: "Get Department server error"})
+    }
+}
+
+const updateDepartment = async(req,res)=>{
+  try{
+    const {dep_name , description }= req.body;
+    const {id} = req.params;
+    const updateDep = await Department.findByIdAndUpdate({_id:id},{
+        dep_name,
+        description
+    })
+    return res.status(200).json({success:true,updateDep})
+  }
+  catch(error){
+    return res.status(500).json({success:false,eerror: "Edit Department server error"})
+
+  }
+}
+
+const deleteDepartment = async (req,res) => {
+    try{
+        const {dep_name , description }= req.body;
+        const {id} = req.params;
+        const deleteDep = await Department.findByIdAndDelete({_id:id})
+        return res.status(200).json({success:true,deleteDep})
+      }
+      catch(error){
+        return res.status(500).json({success:false,eerror: "Delete Department server error"})
+    
+      }
+}
+
+export {addDepartment , getDepartments , getDepartment , updateDepartment , deleteDepartment}
