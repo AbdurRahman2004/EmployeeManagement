1f45dd69c6e7721adad4ba09e4447ee441cd5605
AbdurRahman2004 <ar978874@gmail.com>
2024-11-10 19:11:58 +0530

    final 0.5

--- server/index.js
+++ server/index.js
@@ -35,6 +35,8 @@ app.use(cors({
 }));
 app.use(express.json());
 //app.use(express.static('public/uploads'));
+console.log('Serving static files from:', path.join(__dirname, 'public/uploads'));
+
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
 app.use('/api/auth', authRouter);
