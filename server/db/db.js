406fdb4e3399db5997404f6179d9a28285a549e3
AbdurRahman2004 <ar978874@gmail.com>
2024-11-10 13:37:06 +0530

    Mongo DB issue

--- server/db/db.js
+++ server/db/db.js
@@ -5,12 +5,22 @@ dotenv.config(); // Load environment variables from .env file
 
 const dbUrl = process.env.MONGODB_URL;
 
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected to the database');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('Mongoose connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+});
+
+// Function to connect to the database
 const connectToDatabase = async () => {
     try {
-        await mongoose.connect(dbUrl,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }); // Add options for compatibility
+        await mongoose.connect(dbUrl);
         console.log("Database connected successfully");
     } catch (error) {
         console.log("Database connection error:", error);
