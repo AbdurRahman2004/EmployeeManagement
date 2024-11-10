f5aafced3ad442295781510b6ab0fb86c64f5c37
AbdurRahman2004 <ar978874@gmail.com>
2024-11-10 17:36:49 +0530

    Product Deployment .1

--- server/db/db.js
+++ server/db/db.js
@@ -8,12 +8,7 @@ const dbUrl = process.env.MONGODB_URL;
 // Function to connect to the database
 const connectToDatabase = async () => {
     try {
-        await mongoose.connect(dbUrl, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            connectTimeoutMS: 1000000,  // 30 seconds
-            serverSelectionTimeoutMS: 1000000,  // 30 seconds
-        });
+        await mongoose.connect(dbUrl);
         console.log("Database connected successfully");
     } catch (error) {
         console.log("Database connection error:", error);
